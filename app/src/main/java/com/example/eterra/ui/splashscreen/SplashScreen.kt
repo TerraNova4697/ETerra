ff363fe43e8316cfc146235c80c2b514d6eb86bb
Nikita Bechthold <nikitabechthold@gmail.com>
2022-04-05 14:05:28 +0600

    Implemented SplashScreen

---
+++ app/src/main/java/com/example/eterra/ui/splashscreen/SplashScreen.kt
@@ -0,0 +1,36 @@
+package com.example.eterra.ui.splashscreen
+
+import android.content.Intent
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowInsets
+import android.view.WindowManager
+import androidx.lifecycle.lifecycleScope
+import com.example.eterra.MainActivity
+import com.example.eterra.R
+import kotlinx.coroutines.delay
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+
+        lifecycleScope.launchWhenCreated {
+            delay(1000)
+            startActivity(Intent(this@SplashScreen, MainActivity::class.java))
+            finish()
+        }
+    }
+}
