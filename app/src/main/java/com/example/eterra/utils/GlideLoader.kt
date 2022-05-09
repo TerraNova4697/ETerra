b21fd3dd5bd9f7644371b9ab5ae0dd5720d89b01
Nikita Bechthold <nikitabechthold@gmail.com>
2022-05-09 21:46:22 +0600

    Adding profile photo with glide

---
+++ app/src/main/java/com/example/eterra/utils/GlideLoader.kt
@@ -0,0 +1,28 @@
+package com.example.eterra.utils
+
+import android.content.Context
+import android.net.Uri
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.example.eterra.R
+import dagger.hilt.android.qualifiers.ActivityContext
+import java.io.IOException
+import javax.inject.Inject
+import javax.inject.Singleton
+
+class GlideLoader (private val context: Context) {
+
+    fun loadUserPicture(imageUri: Uri, imageView: ImageView) {
+        try {
+            Glide
+                .with(context)
+                .load(imageUri)
+                .centerCrop()
+                .placeholder(R.drawable.ic_user_placeholder)
+                .into(imageView)
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+
+}
