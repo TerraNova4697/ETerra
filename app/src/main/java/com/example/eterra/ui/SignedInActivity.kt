3c0d0a904a84d631bb1242ce885d1ad2e1947144
Nikita Bechthold <nikitabechthold@gmail.com>
2022-05-09 12:06:59 +0600

    Checking if profile is completed and navigating correspondingly

--- app/src/main/java/com/example/eterra/ui/SignedInActivity.kt
+++ app/src/main/java/com/example/eterra/ui/SignedInActivity.kt
@@ -2,7 +2,10 @@ package com.example.eterra.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import com.example.eterra.R
+import com.example.eterra.ui.main.MainFragmentDirections
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -10,5 +13,15 @@ class SignedInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signed_in)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.signed_in_nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        val profileCompleted = intent.getIntExtra("profile_completed", 0)
+        if (profileCompleted == 0) {
+            val action = MainFragmentDirections.actionMainFragmentToUserProfileFragment()
+            navController.navigate(action)
+        }
     }
 }
