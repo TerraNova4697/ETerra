fc711e7a4f8f588ead6cbbf4a8bf657e2b1d19ac
Nikita Bechthold <nikitabechthold@gmail.com>
2022-05-11 11:06:04 +0600

    Implemented navigation in SingedInActivity

--- app/src/main/java/com/example/eterra/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/eterra/ui/main/MainFragment.kt
@@ -1,12 +1,31 @@
 package com.example.eterra.ui.main
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.example.eterra.R
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
 
 @AndroidEntryPoint
 class MainFragment: Fragment(R.layout.fragment_main) {
 
+    private val mainViewModel: MainViewModel by viewModels()
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+
+        lifecycleScope.launchWhenCreated {
+            mainViewModel.mainViewModelEvents.collect { event ->
+//                when (event) {
+//                    is
+//                }
+            }
+        }
+    }
 
 }
