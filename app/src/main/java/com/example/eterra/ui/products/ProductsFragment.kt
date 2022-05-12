760dd43cce6eb931dc38c19f6e3fe7e3d54a88a5
Nikita Bechthold <nikitabechthold@gmail.com>
2022-05-12 12:51:03 +0600

    Products menu

--- app/src/main/java/com/example/eterra/ui/products/ProductsFragment.kt
+++ app/src/main/java/com/example/eterra/ui/products/ProductsFragment.kt
@@ -1,17 +1,27 @@
 package com.example.eterra.ui.products
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import com.example.eterra.R
+import com.example.eterra.ui.dashboard.DashboardFragmentDirections
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class ProductsFragment: Fragment(R.layout.fragment_products) {
 
     private val productsViewModel: ProductsViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -25,5 +35,17 @@ class ProductsFragment: Fragment(R.layout.fragment_products) {
         }
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.products_menu, menu)
+    }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_add_product -> {
+                val action = ProductsFragmentDirections.actionItemProductsToAddProductFragment()
+                findNavController().navigate(action)
+                true
+            } else -> return super.onOptionsItemSelected(item)
+        }
+    }
 }
