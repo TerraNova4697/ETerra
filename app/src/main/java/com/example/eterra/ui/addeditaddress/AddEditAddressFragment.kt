c0abe4ebc78f39b1f96bcb22457257453c3cbd93
Nikita Bechthold <nikitabechthold@gmail.com>
2022-05-19 13:14:42 +0600

    Introduced AddAddressFragment

---
+++ app/src/main/java/com/example/eterra/ui/addeditaddress/AddEditAddressFragment.kt
@@ -0,0 +1,7 @@
+package com.example.eterra.ui.addeditaddress
+
+import com.example.eterra.R
+import com.example.eterra.ui.BaseFragment
+
+class AddEditAddressFragment: BaseFragment(R.layout.fragment_add_edit_fragment) {
+}
--- app/src/main/java/com/example/eterra/ui/addresslist/AddressListFragment.kt
+++ app/src/main/java/com/example/eterra/ui/addresslist/AddressListFragment.kt
@@ -1,7 +1,26 @@
 package com.example.eterra.ui.addresslist
 
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.findNavController
 import com.example.eterra.R
+import com.example.eterra.databinding.FragmentAddressListBinding
 import com.example.eterra.ui.BaseFragment
 
 class AddressListFragment: BaseFragment(R.layout.fragment_address_list) {
+
+    private lateinit var binding: FragmentAddressListBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentAddressListBinding.bind(view)
+
+        binding.apply {
+            tvAddAddress.setOnClickListener {
+                val action = AddressListFragmentDirections.actionAddressListFragmentToAddEditAddressFragment()
+                findNavController().navigate(action)
+            }
+        }
+    }
+
 }
