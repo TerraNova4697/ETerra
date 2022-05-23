9d68d11ff6d2dcd13fba0964cd65dc0e63e54c85
Nikita Bechthold <nikitabechthold@gmail.com>
2022-05-23 17:30:20 +0600

    Introduced new destination in bottom navigation

---
+++ app/src/main/java/com/example/eterra/ui/soldproducts/SoldProductsFragment.kt
@@ -0,0 +1,7 @@
+package com.example.eterra.ui.soldproducts
+
+import com.example.eterra.R
+import com.example.eterra.ui.BaseFragment
+
+class SoldProductsFragment: BaseFragment(R.layout.sold_products_fragment) {
+}
